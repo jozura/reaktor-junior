135082f95e34a6f857cf5b7c8cb4d200511b99ef
jozura <johannes.haapakoski@outlook.com>
2021-02-07 20:49:35 +0200

    Changed it so that database update is executed as a single transaction.

--- proxy-server/src/updateDB.js
+++ proxy-server/src/updateDB.js
@@ -2,6 +2,7 @@
 
 const {composeData} = require('./composeData')
 const redis = require('redis');
+const PRODUCT_CATEGORIES = require('./constants').PRODUCT_CATEGORIES;
 const {promisify} = require('util');
 
 const redisClient = redis.createClient({
@@ -10,13 +11,9 @@ const redisClient = redis.createClient({
     password: process.env.REDIS_PASS
 });
 
-const hsetAsync = promisify(redisClient.hset).bind(redisClient);
 const smembersAsync = promisify(redisClient.smembers).bind(redisClient);
-const unlinkAsync = promisify(redisClient.unlink).bind(redisClient);
-const hdelAsync = promisify(redisClient.hdel).bind(redisClient);
-const saddAsync = promisify(redisClient.sadd).bind(redisClient);
 
-async function removeDeletedProducts(category, newIds){
+async function removeDeletedProducts(category, newIds, commandQueue){
     let previousIds = await smembersAsync(`prev:${category}`);
 
     let prevSet = new Set(previousIds);
@@ -25,61 +22,80 @@ async function removeDeletedProducts(category, newIds){
     // Set difference (prevSet / newSet)
     let deletedIds = new Set([...prevSet].filter(id => !newSet.has(id)));
 
-    let promises = [];
     deletedIds.forEach((id) => {
-        promises.push(hdelAsync(category, id));
+        commandQueue.hdel(category, id);
     });
 
-    await Promise.allSettled(promises);
+    return commandQueue;
 }
 
-async function updatePreviousIds(category, newIds) {
-     await unlinkAsync(`prev:${category}`);
+function updatePreviousIds(category, newIds, commandQueue) {
+    commandQueue.unlink(`prev:${category}`);
 
-    let newPromises = [];
     newIds.forEach(id => {
-        newPromises.push(saddAsync(`prev:${category}`, id)); 
+        commandQueue.sadd(`prev:${category}`, id); 
     });
 
-    await Promise.allSettled(newPromises);
+    return commandQueue
 }
 
-async function storeData(productData){
-    let promises = [];
-    for(productCategory of productData){
-        let ids = [];
-        category = productCategory[0].type;
-        productCategory.forEach(product => {
-            let id = product.id;
-            ids.push(id);
-            let category = product.type;
-            promises.push(hsetAsync(category,
-                             id,
-                             JSON.stringify(product))
-            );
-        });
-        await removeDeletedProducts(category, ids);
-        await updatePreviousIds(category, ids);
+function getProductIds(productCategory){
+    let ids = [];
+    productCategory.forEach(product => {
+        ids.push(product.id);
+    });
+
+    return ids;
+}
+
+function storeProducts(productCategory, categoryName, commandQueue) {
+    productCategory.forEach(product => {
+        commandQueue.hset(categoryName,
+                         product.id,
+                         JSON.stringify(product))
+    });
+    
+    return commandQueue
+}
+
+async function queueDbCommands(productData, commandQueue){
+    for(let i = 0; i < productData.length; ++i) {
+        let productCategory = productData[i];
+        let categoryName = PRODUCT_CATEGORIES[i];
+        let ids = getProductIds(productCategory);
+        commandQueue = storeProducts(productCategory, categoryName, commandQueue);
+        commandQueue = await removeDeletedProducts(categoryName, ids, commandQueue);
+        commandQueue = updatePreviousIds(categoryName, ids, commandQueue);
     }
 
-    await Promise.allSettled(promises);
+    return commandQueue
 };
 
 async function main(){
-    let productData;
+    let commandQueue;
     try {
-        productData = await composeData();
+        commandQueue = redisClient.multi();
+        let productData = await composeData();
+        commandQueue = await queueDbCommands(productData, commandQueue);
     } catch (error) {
         console.error(error);
         redisClient.quit();
         // In this case do something in the scheduler
         process.exit(1);
     }
-         
-    await storeData(productData);
+    
 
-    redisClient.quit();
-    process.exit(0);
+    commandQueue.exec(function (error, results) {
+        if(error) {
+            console.error(error);
+            redisClient.quit();
+            // In this case do something in the scheduler
+            process.exit(1);
+        }
+        redisClient.quit();
+        process.exit(0);
+
+    })
 }
 
 main();
