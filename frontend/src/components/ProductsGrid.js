a3c06c2fb752e622f6c422439220034eb8298427
jozura <johannes.haapakoski@outlook.com>
2021-02-11 17:35:40 +0200

    Little cosmetic change on datatable.

--- frontend/src/components/ProductsGrid.js
+++ frontend/src/components/ProductsGrid.js
@@ -3,11 +3,11 @@ import {AutoSizer} from "react-virtualized";
 import * as constants from "./constants";
 
 const properties = constants.PRODUCT_PROPERTIES; 
-const columnWidths = [150, 200, 300, 100, 100, 150];
+const columnWidths = [275, 150, 250, 100, 75, 150];
 const gridHeaderHeight = 20;
 
 const Cell = ({ data, columnIndex, rowIndex, style }) => { 
-    let value = data[rowIndex][properties[columnIndex]]
+    let value = data[rowIndex][properties[columnIndex]];
     if (columnIndex === 5) value = value.join(', ');
     return (
     <div style={style} className={rowIndex % 2 ? "GridRowOdd" : "GridRowEven"}>
@@ -16,23 +16,23 @@ const Cell = ({ data, columnIndex, rowIndex, style }) => {
 }
 export default function ProductsGrid({products}) {
     if(products) {
-    return (
-    <AutoSizer> 
-    {({ height, width }) => (
-        <Grid
-           className="Grid"
-           columnCount={6}
-           columnWidth={index => columnWidths[index]}
-           height={height - gridHeaderHeight}
-           rowCount={products.length}
-           rowHeight={() => 50}
-           width={width}
-           itemData={products}
-           overscanColumnCount={10}
-        >
-        {Cell}
-        </Grid>
-    )}
-    </AutoSizer>);
+        return (
+        <AutoSizer> 
+        {({ height, width }) => (
+            <Grid
+               className="Grid"
+               columnCount={6}
+               columnWidth={index => columnWidths[index]}
+               height={height - gridHeaderHeight}
+               rowCount={products.length}
+               rowHeight={() => 50}
+               width={width}
+               itemData={products}
+               overscanColumnCount={10}
+            >
+            {Cell}
+            </Grid>
+        )}
+        </AutoSizer>);
     }
 }
